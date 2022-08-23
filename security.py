94edb6c64182141e7c187f7ea4049b1907f2689c
Bello Ridwan Oluwadamilare <ridwanbello718gmail.com>
2022-08-23 14:11:07 -0700

    proper code for criminal profiling

--- security.py
+++ security.py
@@ -1,2 +1,14 @@
 #this is a new python file
 name = "ridwan bello"
+criminal_1 = input("type in first name ")
+crime = input("type in offence ")
+if crime == murder:
+    print('capital punishment')
+elif crime == theft:
+    print('light punishment')
+
+criminals = [['fatai','burglar'], ['clement','kidnapper'], ['goatie', 'bandit']]
+
+criminals.append[0] = ['thunder', 'cultist']
+print("the list of criminals:", criminals)
+
